4741b2b5a9d5e628975701834075331e65c6511e
OoopOoop <anastasiiabaranova@yahoo.co.uk>
2016-04-20 23:15:03 +0100

    commit to find-service
    Display all running services in Console.

---
+++ SilverFox/ConsoleDisplayService/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Management;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace ConsoleDisplayService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceController[] scServices;
+            scServices = ServiceController.GetServices();
+
+            var teamViwerSer = new ServiceController("TeamViewer");
+            if(teamViwerSer.Status==ServiceControllerStatus.Running)
+            {
+                teamViwerSer.Stop();
+                Thread.Sleep(5000);
+                Console.WriteLine("TeamViwer service status: {0}", teamViwerSer.Status);
+            }
+          
+
+            foreach (ServiceController curService in scServices)
+            {
+                if(curService.Status==ServiceControllerStatus.Running)
+                {
+                    Console.WriteLine("Service: {0}", curService.ServiceName);
+                    Console.WriteLine("Display name: {0}", curService.DisplayName);
+                    ManagementObject wmiService;
+                    wmiService = new ManagementObject("Win32_Service.Name='" + curService.ServiceName + "'");
+                    wmiService.Get();
+                    Console.WriteLine("Start name:  {0}", wmiService["StartName"]);
+                    Console.WriteLine("Desctription: {0}", wmiService["Description"]);
+                }
+            }
+
+           
+            if(teamViwerSer.Status==ServiceControllerStatus.Stopped)
+            {
+                teamViwerSer.Start();
+                Thread.Sleep(5000);
+                Console.WriteLine("TeamViwer service status: {0}", teamViwerSer.Status);
+            }
+         
+            Console.ReadLine();
+        }
+    }
+}
