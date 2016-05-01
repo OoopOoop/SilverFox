4cfdb1099c69031c1250168900d2f1221602581d
OoopOoop <anastasiiabaranova@yahoo.co.uk>
2016-05-01 19:35:28 +0100

    commit to find-service
    create ServiceManager object with main methods to control a service

--- SilverFox/Main/Models/ServiceItem.cs
+++ SilverFox/Main/Models/ServiceItem.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management;
+using System.Runtime.Serialization;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,96 +13,16 @@ namespace Main.Models
 {
    public class ServiceItem
     {
-        private ServiceController sController;
-        private ManagementObject wmiService;
-
-        public string ServiceName { get { return sController.ServiceName; } }
-        public ServiceControllerStatus Status { get { return sController.Status; }}
-        public string DisplayName { get { return sController.DisplayName; } }
-        public bool CanStop { get { return sController.CanStop; } }
-
-        public object Description { get { return wmiService["Description"]; } }
-        public object StartMode { get { return wmiService["StartMode"]; } }
-
-
-         public ServiceItem (ServiceController _sController)
-        {
-            sController = _sController;
-            wmiService = new ManagementObject("Win32_Service.Name='" + sController.ServiceName + "'");
-            wmiService.Get();
-        }
-
-
-        public void StartService()
-        {
-            try
-            {
-                sController.Start();
-                sController.WaitForStatus(ServiceControllerStatus.Running);
-            }
-            catch(Exception)
-            {
-
-            }
-        }
-
-
-        public void StopService()
-        {
-            try
-            {
-                sController.Stop();
-                sController.WaitForStatus(ServiceControllerStatus.Stopped);
-            }
-            catch (Exception)
-            {
-
-            }
-        }
-
-
-        public void RestartService()
-        {
-            try
-            {
-                if (sController.CanStop && (sController.Status == ServiceControllerStatus.Running || sController.Status == ServiceControllerStatus.Paused))
-                {
-                    StopService();
-                    sController.WaitForStatus(ServiceControllerStatus.Stopped);
-                }
-
-                if (sController.Status == ServiceControllerStatus.Stopped)
-                {
-                    StartService();
-                    sController.WaitForStatus(ServiceControllerStatus.Running);
-                }
-            }
-            catch (Exception)
-            {
-            }
-        }
-
-
-        public void ChangeSerStartUp(int mode)
-        {
-            ServiceStartMode startMode = 0;
-
-            switch (mode)
-            {
-                case 1:
-                    startMode = ServiceStartMode.Automatic;
-                    break;
-                case 2:
-                    startMode = ServiceStartMode.Disabled;
-                    break;
-                case 3:
-                    startMode = ServiceStartMode.Manual;
-                    break;
-                default:
-                    startMode = ServiceStartMode.Automatic;
-                    break;
-            }
-            ServiceHelper.ChangeStartMode(sController, startMode);
-        }
+        public string ServiceName { get; set; }
+        [IgnoreDataMember]
+        public string Status { get; set; }
+        [IgnoreDataMember]
+        public string DisplayName { get; set; }
+        [IgnoreDataMember]
+        public bool CanStop { get; set; }
+        [IgnoreDataMember]
+        public string Description { get; set;}
+        [IgnoreDataMember]
+        public string StartMode { get; set;}
     }
 }
---
+++ SilverFox/Main/Models/ServiceManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Management;
+using Main.Shared;
+using System.Runtime.Serialization.Json;
+
+
+
+namespace Main.Models
+{
+    //Make it static? (singletom)
+    public static class ServiceManager
+    {
+        public static ServiceController GetService(ServiceItem item)
+        {
+            var ServiceControllerService = ServiceController.GetServices().First((x) => x.ServiceName == item.ServiceName);
+            return ServiceControllerService;
+        }
+
+        public static ManagementObject GetManagementObject(string serviceName)
+        {
+           var wmiService = new ManagementObject("Win32_Service.Name='" + serviceName + "'");
+           wmiService.Get();
+           return wmiService;
+        }
+
+        public static void StartService(ServiceItem item)
+        {
+            //Start
+            var controller=GetService(item);
+            controller.Start();
+            controller.WaitForStatus(ServiceControllerStatus.Running);
+           
+        }
+
+        public static void StopService(ServiceItem item)
+        {
+            //Stop
+            var controller = GetService(item);
+            controller.Stop();
+            controller.WaitForStatus(ServiceControllerStatus.Stopped);
+        }
+
+        public static void RefreshStatus(ServiceItem item)
+        {
+            // call it on startUp
+            // Update value of serviceItem status
+            var controller = GetService(item);
+            controller.Refresh();
+        }
+
+        public static List<ServiceItem> GetAllServiceItems()
+        {
+            //get all currently running services
+            //var items = ServiceController.GetServices().Select(service => new ServiceItem
+            //{
+            //    ServiceName = service.ServiceName,
+            //    DisplayName = service.DisplayName,
+            //    Status = service.Status.ToString(),
+            //    CanStop = service.CanStop,
+            //    Description = GetManagementObject(service.ServiceName)["Description"].ToString(),
+            //    StartMode = GetManagementObject(service.ServiceName)["StartMode"].ToString(),
+            //}).ToList();
+            //return items;
+
+
+
+            List<ServiceItem> runningServices = new List<ServiceItem>();
+            var services = ServiceController.GetServices();
+            foreach (var service in services)
+            {
+                var wmiService = GetManagementObject(service.ServiceName);
+                runningServices.Add(new ServiceItem
+                {
+                    ServiceName = service.ServiceName,
+                    CanStop = service.CanStop,
+                    DisplayName = service.DisplayName,
+                    Status = service.Status.ToString(),
+                    Description = wmiService["Description"].ToString(),
+                    StartMode = wmiService["StartMode"].ToString()
+                }); 
+            }
+
+            return runningServices;
+        }
+
+
+        public static void ChangeStartMode(ServiceItem item, string changeTo)
+        {
+            var controller = GetService(item);
+            ServiceStartMode mode;
+            switch (changeTo)
+            {
+                case "Automatic":
+                     mode=ServiceStartMode.Automatic;
+                        break;
+                case "Manual":
+                    mode = ServiceStartMode.Manual;
+                    break;
+                case "Disabled":
+                    mode = ServiceStartMode.Disabled;
+                    break;
+                default:
+                    mode = ServiceStartMode.Automatic;
+                    break;
+            }
+
+            ServiceHelper.ChangeStartMode(controller, mode);
+        }
+
+        public static List<ServiceItem> GetSavedServiceItems()
+        {
+            //read a file with saved servies
+            return null;
+        }
+
+
+        //Get reference to windows.storage
+        public static void SetSavedServiceItems(List<ServiceItem> items)
+        {
+            //saves all selected items (name)
+
+         // StorageFolder 
+                
+            var jsonSerializer = new DataContractJsonSerializer(typeof(List<ServiceItem>));
+          //  using(var stream =)
+        }
+    }
+}
